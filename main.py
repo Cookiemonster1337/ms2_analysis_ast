ed28de38139c667c25a7c78b46699cf0ba8e61ba
zbt-tools <l.feierabend@zbt-duisburg.de>
2022-08-09 17:03:11 +0200

    0.03

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import pandas as pd
 from plotly.offline import plot
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from datetime import datetime
-import numpy as np
+import os
 
-folder = r'W:\Projekte\MAXCoat_61906\04_Bearbeitung\in-situ PEMFC\MS2\S316LwCoating\testrig'
+# ----------------------------------------------------------------------------------------------------------------------
+# DATA IMPORT
+# ----------------------------------------------------------------------------------------------------------------------
+testrig_folder = r'W:\Projekte\MAXCoat_61906\04_Bearbeitung\in-situ PEMFC\MS2\S316LwCoating\testrig'
+gamry_folder = r'W:\Projekte\MAXCoat_61906\04_Bearbeitung\in-situ PEMFC\MS2\S316LwCoating\gamry'
 
-files = ['maxcoat_ss_coating_#01_20220729.txt',
+# FILES TESTRIG
+
+testrig_files = ['maxcoat_ss_coating_#01_20220729.txt',
          'maxcoat_ss_coating_#01_20220730.txt',
          'maxcoat_ss_coating_#01_20220731.txt',
          'maxcoat_ss_coating_#01_20220801.txt',
@@ -19,100 +24,65 @@ files = ['maxcoat_ss_coating_#01_20220729.txt',
          'maxcoat_ss_coating_#01_20220807.txt',
          'maxcoat_ss_coating_#01_20220808.txt']
 
-dfs = [pd.read_csv(folder + '/' + f, encoding='cp1252', delimiter='\t', decimal=',') for f in files]
-
+dfs = [pd.read_csv(testrig_folder + '/' + f, encoding='cp1252', delimiter='\t', decimal=',') for f in testrig_files]
 df = pd.concat(dfs, ignore_index=True)
+
+
+
+# EIS FILES
+
+eis_files = [f for f in os.listdir(gamry_folder) if 'EIS' in f]
+
+columns = ['index', 'datapoints [#]', 'time [s]', 'frequency [Hz]',
+          'Z_real [Ohm]', 'Z_imag [Ohm]', 'Z_sig [V]', 'Zmod [ohm]',
+          'Z_phz [°C]', 'I_DC [A]', 'V_DC [V]', 'IE_Range [#]']
+eis_dfs = [pd.read_csv(gamry_folder + '/' + f, encoding='cp1252',
+                       delimiter='\t', decimal=',', skiprows=22, dtype=float,
+                       names=columns) for f in eis_files]
+# CV FILES
+cv_files = [f for f in os.listdir(gamry_folder) if 'CV' in f]
+
+# DICTIONARY
+meas_characteristics = {}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# DATAFRAMES
+# ----------------------------------------------------------------------------------------------------------------------
 
 df['timer'] = pd.to_datetime(df['Datum / Uhrzeit'], format='%d.%m.%y %H:%M:%S')
 df = df.set_index('timer')
 starttime = df.index[0]
-
-
-# df = df[(df.index < '2022-07-30 13:48:23') | (df.index > '2022-08-02 09:53:22')]
-
-# duration = 0
-# timestamp1 = '2022-07-30 13:48:23'
-# timestamp2 = '2022-08-02 09:53:22'
-
-# for i in range(0, len(df)-1):
-#     if df.index[i] < datetime.strptime(timestamp1, '%Y-%m-%d %H:%M:%S'):
-#         duration = df.index[i] - starttime
-#         print(duration)
-#         df['t elapsed [s]'] = duration
-#     if df.index[i] > datetime.strptime(timestamp2, '%Y-%m-%d %H:%M:%S') :
-#         df['t elapsed [s]'] = df.index[i] - datetime.strptime(timestamp2, '%Y-%m-%d %H:%M:%S') + duration
-#     else:
-#         df['t elapsed [s]'] = None
-
-
-# time = df['t elapsed [s]']
 timer = df.index
 df = df.reset_index()
-df['current density [A/cm2]'] = round(df['I Summe [A]'] / 25,2)
+df['current density [A/cm2]'] = round(df['I Summe [A]'] / 25, 2)
 
-# parameters fig1
+# CV DATAFRAMES
+cv1_dfs = [pd.read_csv(gamry_folder + '/' + f, encoding='cp1252',
+                       delimiter='\t', decimal=',', skiprows=21, dtype=float, usecols=[1, 2, 3]
+                      ) for f in cv_files if 'CV1.1' in f]
+
+cv2_dfs = [pd.read_csv(gamry_folder + '/' + f, encoding='cp1252',
+                       delimiter='\t', decimal=',', skiprows=21, dtype=float, usecols=[1, 2, 3]
+                      ) for f in cv_files if 'CV1.2' in f]
+
+cv3_dfs = [pd.read_csv(gamry_folder + '/' + f, encoding='cp1252',
+                       delimiter='\t', decimal=',', skiprows=21, dtype=float, usecols=[1, 2, 3]
+                      ) for f in cv_files if 'CV1.3' in f]
+
+cv4_dfs = [pd.read_csv(gamry_folder + '/' + f, encoding='cp1252',
+                       delimiter='\t', decimal=',', skiprows=21, dtype=float, usecols=[1, 2, 3]
+                      ) for f in cv_files if 'CV1.4' in f]
+# ----------------------------------------------------------------------------------------------------------------------
+# FIGURE - MAIN
+# ----------------------------------------------------------------------------------------------------------------------
+
 voltage = df['AI.U.E.Co.Tb.1 [V]']
 temperature = df['AI.T.Air.ST.UUT.out [°C]']
 hfr = df['HFR [mOhm]'].apply(lambda x: x if x != -99 and x < 100 else None)
 current_density = df['current density [A/cm2]']
 
-#get iv-curves
-iv_markers = df.index[df['Kommentar'] == '#IV-CURVE#'].tolist()
-
-eis_markers = df.index[df['Kommentar'] == '#EIS#'].tolist()
-cv_markers = df.index[df['Kommentar'] == '#CV#'].tolist()
-
-
-#fig2
-fig_pol = make_subplots()
-
-
-for i in range(0, len(iv_markers)):
-
-    if i == len(iv_markers) - 1:
-        df_iv = df.iloc[iv_markers[i]:]
-    else:
-        df_iv = df.iloc[iv_markers[i]: iv_markers[i + 1]]
-
-    df_iv = df_iv[df_iv['Kommentar'] == 'ui1_messen']
-
-    current_densities = pd.unique(df_iv['current density [A/cm2]'])
-    #
-    # for i in range(0, len(iv_df)):
-    #     if df['Kommentar'][i] == 'u1_messen':
-    #         pol_date = df.index[i]
-
-    mean_voltages = []
-
-    for j in current_densities:
-        mean_voltages.append(
-            df_iv[df_iv['current density [A/cm2]'] == j]['AI.U.E.Co.Tb.1 [V]'].mean())
-
-
-
-    iv_name = '#' + str(i+1) + ' @ ' + df['Datum / Uhrzeit'][iv_markers[i]]
-
-    fig_pol.add_trace(
-        go.Scatter(x=current_densities, y=mean_voltages, name=iv_name)
-    )
-
-
-# parameters fig2
-df2 = df[df['Kommentar'] == 'ui1_messen']
-current_densities = pd.unique(df2['current density [A/cm2]'])
-
-for i in range(0, len(df)):
-    if df['Kommentar'][i] == 'u1_messen':
-        pol_date = df.index[i]
-
-
-mean_voltages = []
-for j in current_densities:
-    mean_voltages.append(
-        df2[df2['current density [A/cm2]'] == j]['AI.U.E.Co.Tb.1 [V]'].mean())
-
-
-#fig1
+# eis_markers = df.index[df['Kommentar'] == '#EIS#'].tolist()
+# cv_markers = df.index[df['Kommentar'] == '#CV#'].tolist()
 fig_main = make_subplots(specs=[[{"secondary_y": True}]])
 
 fig_main.add_trace(
@@ -138,25 +108,196 @@ fig_main.add_trace(
 fig_main.update_layout(width=1200, height=600)
 
 
+# ----------------------------------------------------------------------------------------------------------------------
+# FIGURE - IV-CURVE
+# ----------------------------------------------------------------------------------------------------------------------
+
+iv_markers = df.index[df['Kommentar'] == '#IV-CURVE#'].tolist()
+
+fig_pol = make_subplots(x_title='current density [A/cm2]', y_title='voltage [V]')
+
+for i in range(0, len(iv_markers)):
+
+    if i == len(iv_markers) - 1:
+        df_iv = df.iloc[iv_markers[i]:]
+    else:
+        df_iv = df.iloc[iv_markers[i]: iv_markers[i + 1]]
+
+    df_iv = df_iv[df_iv['Kommentar'] == 'ui1_messen']
+
+    df_iv = df_iv[df_iv['current density [A/cm2]'] != 1.19]
+
+    current_densities = pd.unique(df_iv['current density [A/cm2]'])
 
 
-#fig2
-# fig_pol = make_subplots(specs=[[{"secondary_y": True}]])
+    mean_voltages = []
+
+    for j in current_densities:
+        mean_voltages.append(
+            df_iv[df_iv['current density [A/cm2]'] == j]['AI.U.E.Co.Tb.1 [V]'].mean())
+
+
+
+    iv_name = '#' + str(i+1) + ' @ ' + df['Datum / Uhrzeit'][iv_markers[i]]
+
+    fig_pol.add_trace(
+        go.Scatter(x=current_densities, y=mean_voltages, name=iv_name, )
+    )
+
+fig_pol.update_layout(width=1200, height=600)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# FIGURE - AST
+# ----------------------------------------------------------------------------------------------------------------------
+
+ast_markers = df.index[df['Kommentar'] == '#AST-CYCLE#'].tolist()
+
+fig_ast = make_subplots(x_title='duration [h]', y_title='current density [A/cm²]')
+fig_deg = make_subplots(x_title='cycle [#]', y_title='mean current density [A/cm²]')
+
+mean_j_400mv = []
+for i in range(0, len(ast_markers)):
+
+    df_ast = df.iloc[ast_markers[i]:iv_markers[i+1]]
+
+
+    exclusions = ['anfahren_10A', 'anfahren_20A', 'ocv', 'anfahren_I_High', 'anfahren_I_Low', 'halten_I_Low',
+                 'halten_I_High']
+
+    df_ast = df_ast[~df_ast['Kommentar'].isin(exclusions)]
+
+    df_ast = df_ast.reset_index()
+
+    current_density = df_ast['current density [A/cm2]']
+
+    ast_start = df_ast['T relativ [min]'][0]
+
+    df_ast['t elapsed [s]'] = (df_ast['T relativ [min]'] - ast_start) / 60
+
+    duration = df_ast['t elapsed [s]']
+
+# PLOT
+
+    ast_name = '#' + str(i) + ' @ ' + df['Datum / Uhrzeit'][ast_markers[i]]
+
+    fig_ast.add_trace(
+        go.Scatter(x=duration, y=current_density, name=ast_name)
+    )
+
+#     mean_j_400mv.append(df_ast[df_ast['Kommentar'] == 'operation@0.4V']['current density [A/cm2]'].mean())
+#     # mean_j_600mv = df_ast[df_ast['Kommentar'] == 'operation@0.6V']['current density [A/cm2]'].mean()
+#     # mean_j_ocv = df_ast[df_ast['Kommentar'] == 'OCV']['current density [A/cm2]'].mean()
 #
-# fig_pol.add_trace(
-#     go.Scatter(x=current_densities, y=mean_voltages, name="mean_voltage [V]"),
-#     secondary_y=False,
+#
+# fig_deg.add_trace(
+#     go.Scatter(x=[0,1,2,3,4,5], y=[mean_j_400mv], name='mean potential @0.4V')
 # )
 
+    # fig_deg.add_trace(
+    #     go.Scatter(x=[i], y=[mean_j_600mV], name=ast_name)
+    # )
 
-# fig_main.add_trace(
-#     go.Scatter(x=timer, y=voltage, name="voltage [V]"),
-#     secondary_y=True,
-# )
+fig_ast.update_layout(width=1200, height=600)
+
+# AST-DEGRADATION
 
 
 
-# dash
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# FIGURE - EIS
+# ----------------------------------------------------------------------------------------------------------------------
+
+fig_eis = make_subplots(x_title='impedance (real) [Ohm*cm2]', y_title='impedance (imag) [Ohm*cm2]')
+
+for i in range(0, len(eis_dfs)):
+    eis_df = eis_dfs[i]
+
+    impedance_real = eis_df['Z_real [Ohm]'] * 25
+    impedance_imag = eis_df['Z_imag [Ohm]'] * -25
+
+    eis_name = eis_files[i][-20:-8]
+
+    fig_eis.add_trace(
+        go.Scatter(x=impedance_real, y=impedance_imag, mode = 'markers', name=eis_name)
+    )
+
+fig_eis.update_layout(width=1200, height=600)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# FIGURE CV
+# ----------------------------------------------------------------------------------------------------------------------
+
+fig_cv1 = make_subplots(x_title='Potential vs. Hydrogen Anode [V]', y_title='Current [A]')
+
+for i in range(0, len(cv1_dfs)):
+    cv_df = cv1_dfs[i]
+
+    voltage = cv_df['voltage [V]'][38571:]*-1
+    current = cv_df['current [A]'][38571:]*-1
+
+    cv_name = cv_files[i][-20:-8]
+
+    fig_cv1.add_trace(
+        go.Scatter(x=voltage, y=current, name=cv_name)
+    )
+
+fig_cv1.update_layout(width=1200, height=600)
+
+fig_cv2 = make_subplots(x_title='Potential vs. Hydrogen Anode [V]', y_title='Current [A]')
+
+for i in range(0, len(cv2_dfs)):
+    cv_df = cv2_dfs[i]
+
+    voltage = cv_df['voltage [V]'][45151:]*-1
+    current = cv_df['current [A]'][45151:]*-1
+
+    cv_name = cv_files[i][-20:-8]
+
+    fig_cv2.add_trace(
+        go.Scatter(x=voltage, y=current, name=cv_name)
+    )
+
+fig_cv2.update_layout(width=1200, height=600)
+
+fig_cv3 = make_subplots(x_title='Potential vs. Hydrogen Anode [V]', y_title='Current [A]')
+
+for i in range(0, len(cv3_dfs)):
+    cv_df = cv3_dfs[i]
+
+    voltage = cv_df['voltage [V]'][47529:]*-1
+    current = cv_df['current [A]'][47529:]*-1
+
+    cv_name = cv_files[i][-20:-8]
+
+    fig_cv3.add_trace(
+        go.Scatter(x=voltage, y=current, name=cv_name)
+    )
+
+fig_cv3.update_layout(width=1200, height=600)
+
+fig_cv4 = make_subplots(x_title='Potential vs. Hydrogen Anode [V]', y_title='Current [A]')
+
+for i in range(0, len(cv4_dfs)):
+    cv_df = cv4_dfs[i]
+
+    voltage = cv_df['voltage [V]'][44885:]*-1
+    current = cv_df['current [A]'][44885:]*-1
+
+    cv_name = cv_files[i][-20:-8]
+
+    fig_cv4.add_trace(
+        go.Scatter(x=voltage, y=current, name=cv_name)
+    )
+
+fig_cv4.update_layout(width=1200, height=600)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# DASH
+# ----------------------------------------------------------------------------------------------------------------------
 import dash
 from dash import dcc
 from dash import html
@@ -164,10 +305,34 @@ from dash import html
 app = dash.Dash()
 
 app.layout = html.Div([
-    html.H1('IGF MAXCoat (in-situ PEMFC Testing)', style={'textAlign': 'center'}),
-    html.Div('Data of Testrig Parameters'),
-    dcc.Graph(id='main-data', figure=fig_main),
-    dcc.Graph(id='pol-data', figure=fig_pol),
+    html.H1('IGF MAXCoat (in-situ PEMFC-AST Testing)', style={'textAlign': 'center'}),
+
+    html.Div(['Data of Testrig Parameters',
+    dcc.Graph(id='main-data', figure=fig_main),]),
+
+    html.Div(['AST Load Cycling',
+    dcc.Graph(id='ast-data', figure=fig_ast),]),
+
+    html.Div(['AST Load Cycling',
+    dcc.Graph(id='deg-data', figure=fig_deg),]),
+
+    html.Div(['IV-Curve in between AST',
+    dcc.Graph(id='pol-data', figure=fig_pol),]),
+
+    html.Div(['EIS in between AST',
+    dcc.Graph(id='eis-data', figure=fig_eis),]),
+
+    html.Div(['CV-I in between AST (-0.05 to -0.9V @ 100mV/s)',
+              dcc.Graph(id='cv1-data', figure=fig_cv1), ]),
+
+    html.Div(['CV-II in between AST (0 to -0.9V @ 100mV/s)',
+              dcc.Graph(id='cv2-data', figure=fig_cv2), ]),
+
+    html.Div(['CV-III in between AST  (0 to -0.9V @ 20mV/s)',
+              dcc.Graph(id='cv3-data', figure=fig_cv3), ]),
+
+    html.Div(['CV-IV in between AST  (-0.05 to -0.9V @ 20mV/s)',
+              dcc.Graph(id='cv4-data', figure=fig_cv4), ]),
 ])
 
 app.run_server(debug=True, port=8050, host='0.0.0.0')
